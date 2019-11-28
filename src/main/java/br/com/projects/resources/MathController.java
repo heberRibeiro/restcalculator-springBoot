1f4516d781787da76061a59049792e8ec71197be
Heber Ribeiro <heber.r@outlook.com>
2019-11-28 14:19:32 -0300

    Implementado recursos/controle para demais operações

--- src/main/java/br/com/projects/resources/MathController.java
+++ src/main/java/br/com/projects/resources/MathController.java
@@ -21,6 +21,60 @@ public class MathController {
 		return sum;
 	}
 
+	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsuportedOperationException("Unsuported operation. Set a number value.");
+		}
+		Double sub = convertDouble(numberOne) - convertDouble(numberTwo);
+		return sub;
+	}
+
+	@RequestMapping(value = "/mul/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double mul(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsuportedOperationException("Unsuported operation. Set a number value.");
+		}
+		Double mul = convertDouble(numberOne) * convertDouble(numberTwo);
+		return mul;
+	}
+
+	@RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsuportedOperationException("Unsuported operation. Set a number value.");
+		}
+		Double div = convertDouble(numberOne) / convertDouble(numberTwo);
+		return div;
+	}
+
+	@RequestMapping(value = "/med/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double med(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsuportedOperationException("Unsuported operation. Set a number value.");
+		}
+		Double med = (convertDouble(numberOne) / convertDouble(numberTwo)) / 2;
+		return med;
+	}
+
+	@RequestMapping(value = "/sqrt/{number}", method = RequestMethod.GET)
+	public Double sqrt(@PathVariable("number") String number) throws Exception {
+
+		if (!isNumeric(number)) {
+			throw new UnsuportedOperationException("Unsuported operation. Set a number value.");
+		}
+		Double sqrt = Math.sqrt(convertDouble(number));
+		return sqrt;
+	}
+
 	private Double convertDouble(String strNumber) {
 		if (strNumber == null)
 			return 0D;
