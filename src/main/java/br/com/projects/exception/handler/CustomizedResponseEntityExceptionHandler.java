a0344c42d4dcafa9d760fc8d1057259bac6e2c94
Heber Ribeiro <heber.r@outlook.com>
2019-11-28 14:04:22 -0300

    Tratamento de exceções

---
+++ src/main/java/br/com/projects/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,32 @@
+package br.com.projects.exception.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.projects.exception.ExceptionResponse;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllException(Exception exception, WebRequest webReq) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), exception.getMessage(),
+				webReq.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(UnsupportedOperationException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception exception, WebRequest webReq) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), exception.getMessage(),
+				webReq.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+}
--- src/main/java/br/com/projects/resources/MathController.java
+++ src/main/java/br/com/projects/resources/MathController.java
@@ -5,14 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projects.exception.UnsuportedOperationException;
+
 @RestController
 public class MathController {
 
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
 
 		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new UnsuportedOperationException("Unsuported operation. Set a number value.");
 		}
 		Double sum = convertDouble(numberOne) + convertDouble(numberTwo);
 		return sum;
