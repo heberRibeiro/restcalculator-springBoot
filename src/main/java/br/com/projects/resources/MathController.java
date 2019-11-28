ac43446d9fb46ef6da78f58e28081da672d86255
Heber Ribeiro <heber.r@outlook.com>
2019-11-28 13:41:18 -0300

    Implementado recurso/controle para soma

--- src/main/java/br/com/projects/resources/MathController.java
+++ src/main/java/br/com/projects/resources/MathController.java
@@ -9,8 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
-		return 1D;
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		Double sum = convertDouble(numberOne) + convertDouble(numberTwo);
+		return sum;
 	}
 
+	private Double convertDouble(String strNumber) {
+		if (strNumber == null)
+			return 0D;
+		if (isNumeric(strNumber))
+			return Double.parseDouble(strNumber);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null)
+			return false;
+		return strNumber.replaceAll(",", ".").matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
 }
